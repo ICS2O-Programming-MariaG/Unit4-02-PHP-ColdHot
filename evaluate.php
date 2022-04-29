04d2d2e3cafb7d0331cd5682a5344da713896596
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-29 23:53:08 +0000

    Added constant name

--- evaluate.php
+++ evaluate.php
@@ -1,9 +1,10 @@
 <?php
-  //initializing variables
+  //initializing variables & constant
   $temperature = floatval($_POST["temp-entered"]);
+  define (TEMP_THRESHOLD, 15);
   $message = "";
   //if/then statement: if temperature is greater or equal to 15 degrees
-  if ($temperature >= 15) {
+  if ($temperature >= TEMP_THRESHOLD) {
     $message = "It is relatively hot out! You probably won't need a jacket, but you might want to wear sunscreen or a baseball cap.";
   }
   //else represents a temperature less than 15 degrees
